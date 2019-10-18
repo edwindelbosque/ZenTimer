b44bdc993e387ce84153414f4e34716fefc71dac
Edwin Del Bosque <edwindelbosquer@gmail.com>
2019-10-17 18:15:06 -0600

    Add functionality to submit the form with inputs and reset form

--- main.js
+++ main.js
@@ -6,17 +6,41 @@ const buttonMeditate = document.querySelector('#button-meditate');
 const buttonExercise = document.querySelector('#button-exercise');
 const buttonSubmit = document.querySelector('#button-submit');
 const form = document.querySelector('form');
+const formData = {
+  intention: undefined,
+  category: undefined,
+  minutes: undefined,
+  seconds: undefined
+};
+
+const submitForm = (event) => {
+  if (inputIntention.value && inputMinutes.value && inputSeconds.value) {
+    formData.intention = inputIntention.value;
+    formData.minutes = parseInt(inputMinutes.value);
+    formData.seconds = parseInt(inputSeconds.value);
+    clearForm(event);
+  }
+}
+
+const clearForm = (event) => {
+  const buttons = [buttonStudy, buttonMeditate, buttonExercise];
+  const formElements = [inputIntention, inputMinutes, inputSeconds];
+  const formKeys = Object.keys(formData);
+  buttons.forEach(button => button.classList.remove("active"));
+  formElements.forEach(element => element.value = '');
+  formKeys.forEach(key => formData[key] = undefined);
+}
 
 const handleFormButtons = event => {
   event.preventDefault(event);
   if (event.target.id === 'button-study') {
-    console.log('study')
+    toggleButton(event);
   } else if (event.target.id === 'button-meditate') {
-    console.log('medi')
+    toggleButton(event);
   } else if (event.target.id === 'button-exercise') {
-    console.log('exer')
+    toggleButton(event);
   } else if (event.target.id === 'button-submit') {
-    console.log('submit')
+    submitForm(event);
   }
 }
 
@@ -31,6 +55,13 @@ const handleFormInputs = event => {
   }
 }
 
+const toggleButton = event => {
+  buttonStudy.classList.remove("active");
+  buttonMeditate.classList.remove("active");
+  buttonExercise.classList.remove("active");
+  event.target.classList.add("active");
+  formData.category = event.target.value;
+}
 
 form.addEventListener('click', handleFormButtons);
 form.addEventListener('keyup', handleFormInputs);
